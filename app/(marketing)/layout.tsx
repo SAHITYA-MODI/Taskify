921a0d6501ee73efdec1a9b379576f0403599cb7
Sahitya Modi <sahityamodi0@gmail.com>
2024-01-25 01:34:16 +0530

    Created Marketing page UI

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,9 @@
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full">
+      <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+    </div>
+  );
+};
+
+export default MarketingLayout;

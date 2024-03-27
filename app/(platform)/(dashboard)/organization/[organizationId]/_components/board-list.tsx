1bb891ea6cb1b1575ce4daaf4172f13ac9a516c7
Sahitya Modi <sahityamodi0@gmail.com>
2024-03-27 16:50:44 +0530

    Crete Board component ui completed, added server action

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -0,0 +1,31 @@
+import { FormPopover } from "@/components/form/form-popover";
+import { Hint } from "@/components/hint";
+import { HelpCircle, User2 } from "lucide-react";
+
+export const BoardList = () => {
+  return (
+    <div className="space-y-2">
+      <div className="flex items-center text-neutral-700 text-lg font-semibold">
+        <User2 className="h-6 w-6 mr-2" />
+        Your Boards
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        <FormPopover sideOffset={10} side="right">
+          <div
+            role="button"
+            className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col items-center justify-center gap-y-1 hover:opacity-75 transition"
+          >
+            <p className="text-sm">Create new board</p>
+            <span className="text-xs">5 Remaining</span>
+            <Hint
+              sideOffset={40}
+              description={`Free Workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace.`}
+            >
+              <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
+            </Hint>
+          </div>
+        </FormPopover>
+      </div>
+    </div>
+  );
+};

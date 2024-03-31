506478b1e1acc7cfcdf986c0e99524ebfa8ca1da
Sahitya Modi <sahityamodi0@gmail.com>
2024-04-01 01:11:33 +0530

    Copy list and Delete list server actions and UI completed

---
+++ actions/update-list/schema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const UpdateList = z.object({
+  title: z
+    .string({
+      invalid_type_error: "Title is required",
+      required_error: "Title is required",
+    })
+    .min(3, {
+      message: "Title is too short",
+    }),
+  id: z.string(),
+  boardId: z.string(),
+});
